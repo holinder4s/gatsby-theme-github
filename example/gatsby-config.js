55e3c2db2113c0e6c54eddbd3bf15fa722fa70c2
holinder4s <holinder4s@gmail.com>
2021-12-12 19:57:28 +0900

    building-a-theme tutorial

--- example/gatsby-config.js
+++ example/gatsby-config.js
@@ -1,3 +1,11 @@
 module.exports = {
-  plugins: [{ resolve: `gatsby-theme-minimal`, options: {} }],
+  plugins: [
+    {
+      resolve: `gatsby-theme-github`,
+      options: {
+        contentPath: "events",
+        basePath: "/events",
+      },
+    },
+  ],
 }
